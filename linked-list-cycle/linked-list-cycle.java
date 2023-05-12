c0ae0dd99c5192d340e1bb28822e66631d06c4a4
g4dalcom <93110733+g4dalcom@users.noreply.github.com>
2023-05-13 01:35:23 +0900

    Time: 4 ms, Memory: 43.4 MB - LeetHub

--- linked-list-cycle/linked-list-cycle.java
+++ linked-list-cycle/linked-list-cycle.java
@@ -9,20 +9,37 @@
  *     }
  * }
  */
+// public class Solution {
+//     public boolean hasCycle(ListNode head) {
+//         if (head == null || head.next == null) return false;
+        
+//         ListNode walker = head;
+//         ListNode runner = head;
+        
+//         while (runner != null && runner.next != null) {
+//             walker = walker.next;
+//             runner = runner.next.next;
+            
+//             if (walker == runner) return true;
+//         }
+        
+//         return false;
+//     }
+// }
+
+
 public class Solution {
     public boolean hasCycle(ListNode head) {
         if (head == null || head.next == null) return false;
         
-        ListNode walker = head;
-        ListNode runner = head;
-        
-        while (runner != null && runner.next != null) {
-            walker = walker.next;
-            runner = runner.next.next;
-            
-            if (walker == runner) return true;
+        HashSet<ListNode> hs = new HashSet<>();
+        while (head != null) {
+            if (hs.contains(head)) return true;
+            else {
+                hs.add(head);
+                head = head.next;
+            }
         }
-        
-        return false;
+        return false;   
     }
 }
