f9908ed9db79dbebb8cce81396bef616db010a0f
g4dalcom <93110733+g4dalcom@users.noreply.github.com>
2023-05-13 01:35:52 +0900

    Update linked-list-cycle.java

--- linked-list-cycle/linked-list-cycle.java
+++ linked-list-cycle/linked-list-cycle.java
@@ -9,25 +9,27 @@
  *     }
  * }
  */
-// public class Solution {
-//     public boolean hasCycle(ListNode head) {
-//         if (head == null || head.next == null) return false;
+
+// Two Pointer
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) return false;
         
-//         ListNode walker = head;
-//         ListNode runner = head;
+        ListNode walker = head;
+        ListNode runner = head;
         
-//         while (runner != null && runner.next != null) {
-//             walker = walker.next;
-//             runner = runner.next.next;
+        while (runner != null && runner.next != null) {
+            walker = walker.next;
+            runner = runner.next.next;
             
-//             if (walker == runner) return true;
-//         }
+            if (walker == runner) return true;
+        }
         
-//         return false;
-//     }
-// }
+        return false;
+    }
+}
 
-
+// HashSet
 public class Solution {
     public boolean hasCycle(ListNode head) {
         if (head == null || head.next == null) return false;
