aa5828cfcdc65def11f9571dd02915bda6b706ae
g4dalcom <93110733+g4dalcom@users.noreply.github.com>
2023-05-18 10:52:34 +0900

    Time: 1 ms, Memory: 45.1 MB - LeetHub

--- intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -9,16 +9,51 @@
  *     }
  * }
  */
-public class Solution {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode p1 = headA;
-        ListNode p2 = headB;
+// public class Solution {
+//     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//         ListNode p1 = headA;
+//         ListNode p2 = headB;
         
-        while (p1 != p2) {
-            p1 = p1 == null ? headB : p1.next;
-            p2 = p2 == null ? headA : p2.next;
+//         while (p1 != p2) {
+//             p1 = p1 == null ? headB : p1.next;
+//             p2 = p2 == null ? headA : p2.next;
+//         }
+        
+//         return p1;
+//     }
+
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA,ListNode headB) {
+        int Alength = getLength(headA);
+        int Blength = getLength(headB);
+        
+        while (Alength < Blength) {
+            headB = headB.next;
+            Blength--;
         }
         
-        return p1;
+        while (Alength > Blength) {
+            headA = headA.next;
+            Alength--;
+        }
+        
+        while (headA != headB) {
+            headA = headA.next;
+            headB = headB.next;
+        }
+        
+        return headA;
+    }
+    
+    public int getLength(ListNode x) {
+        int cnt = 0;
+
+        while (x != null) {
+            x = x.next;
+            cnt++;    
+        }
+
+        return cnt;
     }
 }
+    
