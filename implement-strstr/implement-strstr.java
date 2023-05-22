9fa6a7f338a25fa9cb447e41a8361d5e52e36925
g4dalcom <93110733+g4dalcom@users.noreply.github.com>
2023-05-22 15:07:59 +0900

    Time: 0 ms, Memory: 40.8 MB - LeetHub

--- implement-strstr/implement-strstr.java
+++ implement-strstr/implement-strstr.java
@@ -1,5 +1,26 @@
+// class Solution {
+//     public int strStr(String haystack, String needle) {
+//         return haystack.indexOf(needle);
+//     }
+// }
+
+
 class Solution {
     public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+        int hLength = haystack.length();
+        int nLength = needle.length();
+        
+        for (int i = 0; i < hLength - nLength + 1; i++) {
+            int cnt = 0;
+            
+            while (cnt < nLength) {
+                if (haystack.charAt(i+cnt) == needle.charAt(cnt)) cnt++;
+                else break;
+            }
+            
+            if (cnt == nLength) return i;
+        }
+        
+        return -1;
     }
 }
