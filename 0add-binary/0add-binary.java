2035cbca49e13da7e58d4a7c4ae1b8927023b26a
g4dalcom <93110733+g4dalcom@users.noreply.github.com>
2023-05-22 09:12:14 +0900

    Time: 1 ms, Memory: 41.4 MB - LeetHub

--- 0add-binary/0add-binary.java
+++ 0add-binary/0add-binary.java
@@ -1,54 +1,29 @@
 class Solution {
     public String addBinary(String a, String b) {
-        Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
         
         int ap = a.length()-1;
         int bp = b.length()-1;
         
         int ceil = 0;
-        while(true) {
-            if (ap < 0) {
-                while (bp >= 0) {
-                    int cal = b.charAt(bp) - '0' + ceil;
-                    stack.push(cal % 2);
-                    ceil = cal >= 2 ? 1 : 0;
-                    bp--;
-                }
-                if (ceil == 1) stack.push(ceil);
-                break;
-            } else if (bp < 0) {
-                while (ap >= 0) {
-                    int cal = a.charAt(ap) - '0' + ceil;
-                    stack.push(cal % 2);
-                    ceil = cal >= 2 ? 1 : 0;
-                    ap--;
-                }
-                if (ceil == 1) stack.push(ceil);
-                break;
+        
+        while (ap >= 0 || bp >= 0) {
+            int sum = ceil;
+            if (ap >= 0) {
+                sum += a.charAt(ap) - '0';
+                ap--;
+            }
+            if (bp >= 0) {
+                sum += b.charAt(bp) - '0';
+                bp--;
             }
             
-            int ia = a.charAt(ap) - '0';
-            int ib = b.charAt(bp) - '0';
-            
-            int cal = ia + ib + ceil;
-            
-            if (cal >= 2) {
-                stack.push(cal % 2);
-                ceil = 1;
-            } else {
-                stack.push(cal);
-                ceil = 0;
-            }
-            
-            ap--;
-            bp--;
+            sb.append(sum >= 2 ? sum % 2 : sum);
+            ceil = sum / 2;
         }
         
-        StringBuilder sb = new StringBuilder();
-        while (!stack.isEmpty()) {
-            sb.append(stack.pop());
-        }
+        if (ceil != 0) sb.append(ceil);
         
-        return sb.toString();
+        return sb.reverse().toString();
     }
 }
