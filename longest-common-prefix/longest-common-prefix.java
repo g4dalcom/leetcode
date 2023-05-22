5854991394d7b6337ca15cd3c40864d9ec9156ce
g4dalcom <93110733+g4dalcom@users.noreply.github.com>
2023-05-22 16:42:25 +0900

    Time: 1 ms, Memory: 40.3 MB - LeetHub

--- longest-common-prefix/longest-common-prefix.java
+++ longest-common-prefix/longest-common-prefix.java
@@ -1,25 +1,42 @@
+// class Solution {
+//     public String longestCommonPrefix(String[] strs) {
+//         StringBuilder sb = new StringBuilder();
+
+//         for (int i = 0; i < strs[0].length(); i++) {
+//             boolean flag = true;
+//             for (int j = 0; j < strs.length; j++) {
+//                 if (i >= strs[j].length()) {
+//                     flag = false;
+//                     break;
+//                 }
+
+//                 if (strs[0].charAt(i) != strs[j].charAt(i)) {
+//                     flag = false;
+//                     break;
+//                 }
+
+//             }
+//             if (flag) sb.append(strs[0].charAt(i));
+//             else return sb.toString();
+//         }
+        
+//         return sb.toString();
+//     }
+// }
+
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < strs[0].length(); i++) {
-            boolean flag = true;
-            for (int j = 0; j < strs.length; j++) {
-                if (i >= strs[j].length()) {
-                    flag = false;
-                    break;
-                }
-
-                if (strs[0].charAt(i) != strs[j].charAt(i)) {
-                    flag = false;
-                    break;
-                }
-
-            }
-            if (flag) sb.append(strs[0].charAt(i));
-            else return sb.toString();
+        Arrays.sort(strs);
+        
+        String s1 = strs[0];
+        String s2 = strs[strs.length-1];
+        
+        int idx = 0;
+        while (idx < s1.length() && idx < s2.length()) {
+            if (s1.charAt(idx) != s2.charAt(idx)) break;
+            idx++;
         }
         
-        return sb.toString();
+        return s1.substring(0, idx);
     }
 }
