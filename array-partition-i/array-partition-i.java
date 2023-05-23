4c068559dfc31ac2a8aeafc4a8bcba71e64b09c4
g4dalcom <93110733+g4dalcom@users.noreply.github.com>
2023-05-23 11:36:50 +0900

    Time: 7 ms, Memory: 45 MB - LeetHub

--- array-partition-i/array-partition-i.java
+++ array-partition-i/array-partition-i.java
@@ -1,10 +1,34 @@
+// class Solution {
+//     public int arrayPairSum(int[] nums) {
+//         Arrays.sort(nums);
+        
+//         int sum = 0;
+//         for (int i = 0; i < nums.length; i+=2) {
+//             sum += Math.min(nums[i], nums[i+1]);
+//         }
+        
+//         return sum;
+//     }
+// }
+
 class Solution {
     public int arrayPairSum(int[] nums) {
-        Arrays.sort(nums);
+        int[] counting = new int[20001];
+        
+        for (int i = 0; i < nums.length; i++) {
+            counting[nums[i] + 10000]++;
+        }
         
         int sum = 0;
-        for (int i = 0; i < nums.length; i+=2) {
-            sum += Math.min(nums[i], nums[i+1]);
+        boolean odd = true;
+        for (int i = 0; i < counting.length; i++) {
+            while (counting[i]-- > 0) {
+                if (odd) {
+                    sum += i - 10000;
+                }
+                
+                odd = !odd;
+            }
         }
         
         return sum;
