32f7e768aed75b06f9f43e04a2f2d100036ab8ed
g4dalcom <93110733+g4dalcom@users.noreply.github.com>
2023-05-27 11:24:45 +0900

    Time: 1 ms, Memory: 40.5 MB - LeetHub

--- pascals-triangle-ii/pascals-triangle-ii.java
+++ pascals-triangle-ii/pascals-triangle-ii.java
@@ -1,19 +1,35 @@
+// class Solution {
+//     public List<Integer> getRow(int rowIndex) {
+//         int[] pascal = new int[rowIndex+1];
+        
+//         for (int i = 0; i < rowIndex+1; i++) {
+//             int prev = 1;
+//             for (int j = 0; j < i+1; j++) {
+//                 if (j == 0 || j == i) pascal[j] = 1;
+//                 else {
+//                     int temp = pascal[j];
+//                     pascal[j] = prev + pascal[j];
+//                     prev = temp;
+//                 }
+//             }
+//         }
+        
+//         return Arrays.stream(pascal).boxed().collect(Collectors.toList());
+//     }
+// }
+
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-        int[] pascal = new int[rowIndex+1];
+        List<Integer> result = new ArrayList<>();
         
-        for (int i = 0; i < rowIndex+1; i++) {
-            int prev = 1;
-            for (int j = 0; j < i+1; j++) {
-                if (j == 0 || j == i) pascal[j] = 1;
-                else {
-                    int temp = pascal[j];
-                    pascal[j] = prev + pascal[j];
-                    prev = temp;
-                }
+        for (int i = 0; i <= rowIndex; i++) {
+            result.add(1);
+            
+            for (int j = i-1; j > 0; j--) {
+                result.set(j, result.get(j-1) + result.get(j));
             }
         }
         
-        return Arrays.stream(pascal).boxed().collect(Collectors.toList());
+        return result;
     }
 }
