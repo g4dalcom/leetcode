2155fc7cfa3282440baee4216cf3da8f2ddfa8a4
g4dalcom <93110733+g4dalcom@users.noreply.github.com>
2023-06-01 00:28:56 +0900

    Time: 16 ms, Memory: 54.1 MB - LeetHub

--- 0kth-largest-element-in-an-array/0kth-largest-element-in-an-array.java
+++ 0kth-largest-element-in-an-array/0kth-largest-element-in-an-array.java
@@ -1,48 +1,88 @@
+// class Solution {
+//     public int findKthLargest(int[] nums, int k) {
+//         int result = 0;
+        
+//         if (nums.length < 2) {
+//             result = nums[0];
+//             return result;
+//         }
+        
+//         for (int i = nums.length/2 -1; i >= 0; i--) {
+//             heapify(nums, i, nums.length);
+//         }
+        
+        
+//         for (int i = nums.length-1; i >= 0; i--) {
+//             if (--k == 0) {
+//                 result = nums[0];
+//                 break;
+//             }
+//             swap(nums, 0, i);
+//             heapify(nums, 0, i);
+//         }
+        
+//         return result;
+//     }
+    
+//     public void heapify(int[] nums, int parentIdx, int size) {
+//         int left;
+//         int right;
+//         int largest;
+        
+//         while ((parentIdx * 2) + 1 < size) {
+//             left = parentIdx * 2 + 1;
+//             right = parentIdx * 2 + 2;
+//             largest = parentIdx;
+            
+//             if (nums[left] > nums[largest]) largest = left;
+//             if (right < size && nums[right] > nums[largest]) largest = right;
+            
+//             if (largest != parentIdx) {
+//                 swap(nums, parentIdx, largest);
+//                 parentIdx = largest;
+//             }
+//             else return;
+//         }
+//     }
+    
+//     public void swap(int[] nums, int i, int j) {
+//         int temp = nums[i];
+//         nums[i] = nums[j];
+//         nums[j] = temp;
+//     }
+// }
+
+
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        int result = 0;
+        int start = 0;
+        int end = nums.length-1;
+        int target = nums.length - k;
         
-        if (nums.length < 2) {
-            result = nums[0];
-            return result;
+        while (start < end) {
+            int pivot = partition(nums, start, end);
+            if (pivot < target) start = pivot + 1;
+            else if (pivot > target) end = pivot - 1;
+            else return nums[pivot];
         }
         
-        for (int i = nums.length/2 -1; i >= 0; i--) {
-            heapify(nums, i, nums.length);
-        }
-        
-        
-        for (int i = nums.length-1; i >= 0; i--) {
-            if (--k == 0) {
-                result = nums[0];
-                break;
-            }
-            swap(nums, 0, i);
-            heapify(nums, 0, i);
-        }
-        
-        return result;
+        return nums[start];
     }
     
-    public void heapify(int[] nums, int parentIdx, int size) {
-        int left;
-        int right;
-        int largest;
+    public int partition(int[] nums, int start, int end) {
+        int pivot = start;
         
-        while ((parentIdx * 2) + 1 < size) {
-            left = parentIdx * 2 + 1;
-            right = parentIdx * 2 + 2;
-            largest = parentIdx;
+        while (start <= end) {
+            while (start <= end && nums[start] <= nums[pivot]) start++;
+            while (start <= end && nums[end] > nums[pivot]) end--;
             
-            if (nums[left] > nums[largest]) largest = left;
-            if (right < size && nums[right] > nums[largest]) largest = right;
-            
-            if (largest != parentIdx) {
-                swap(nums, parentIdx, largest);
-                parentIdx = largest;
+            if (start <= end) {
+                swap(nums, start, end);
             }
-            else return;
         }
+        swap(nums, end, pivot);
+        return end;
+        
     }
     
     public void swap(int[] nums, int i, int j) {
