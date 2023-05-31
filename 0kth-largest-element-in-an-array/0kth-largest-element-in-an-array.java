9b77f648d188a70b6d89dfc8b5c0faa15cb109f8
g4dalcom <93110733+g4dalcom@users.noreply.github.com>
2023-05-31 23:39:24 +0900

    Time: 26 ms, Memory: 54.3 MB - LeetHub

---
+++ 0kth-largest-element-in-an-array/0kth-largest-element-in-an-array.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        int result = 0;
+        
+        if (nums.length < 2) {
+            result = nums[0];
+            return result;
+        }
+        
+        for (int i = nums.length/2 -1; i >= 0; i--) {
+            heapify(nums, i, nums.length);
+        }
+        
+        
+        for (int i = nums.length-1; i >= 0; i--) {
+            if (--k == 0) {
+                result = nums[0];
+                break;
+            }
+            swap(nums, 0, i);
+            heapify(nums, 0, i);
+        }
+        
+        return result;
+    }
+    
+    public void heapify(int[] nums, int parentIdx, int size) {
+        int left;
+        int right;
+        int largest;
+        
+        while ((parentIdx * 2) + 1 < size) {
+            left = parentIdx * 2 + 1;
+            right = parentIdx * 2 + 2;
+            largest = parentIdx;
+            
+            if (nums[left] > nums[largest]) largest = left;
+            if (right < size && nums[right] > nums[largest]) largest = right;
+            
+            if (largest != parentIdx) {
+                swap(nums, parentIdx, largest);
+                parentIdx = largest;
+            }
+            else return;
+        }
+    }
+    
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
